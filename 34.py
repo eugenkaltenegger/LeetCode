bed8d7f9973204ecd8ab399a9c73fdaf8d7cf33f
Eugen Kaltenegger <eugen.kaltenegger@gmail.com>
2022-07-25 18:26:00 +0200

    removing .md files

--- 34.py
+++ 34.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python3
+
+# Problem description: https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
+
 class Solution(object):
 
     def searchRange(self, nums, target):
--- 70.py
+++ 70.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python3
+
+# Problem description: https://leetcode.com/problems/climbing-stairs/
+
 import math
 
 
